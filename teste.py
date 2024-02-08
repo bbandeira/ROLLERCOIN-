c1497333a5ed9f09bc7d396b29cbd7b02fae7194
bbandeira <brunomedeirosreis@gmail.com>
2024-02-08 18:30:50 -0300

    BOT ROLLERCOIN
    BOT ROLLERCOIN

---
+++ teste.py
@@ -0,0 +1,21 @@
+import os
+import random
+import time
+import datetime
+import math
+import sys
+
+import numpy
+import cv2
+import keyboard
+import pyautogui
+import shutil
+from PIL import ImageGrab
+from MTM import matchTemplates
+from threading import Thread
+while True:
+    print(pyautogui.position())
+    time.sleep(0.3)
+
+    #x=260, y=133
+    #830 x 625
